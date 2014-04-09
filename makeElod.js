ea924244326d5d993ece1f9d433323257e44d095
Bernhard Spindler <bernahrd.spindler.75@gmail.com>
2014-04-09 14:27:30 +0200

    if the inputfile does not exists, quit the script

--- makeElod.js
+++ makeElod.js
@@ -1,11 +1,21 @@
 /*
  *	2014-03-06 Spindler Bernhard - initial
  * 	2014-04-03 Spindler Bernhard - changes WScript.Echo to "Textfile.WriteLine" because of a codepage problem.
+ *	2014-04-09 Spindler Bernhard - if input file doesn't exists, just quit the script.
  */
 //
 //
 //
+var oFso = WScript.CreateObject("Scripting.FileSystemObject");
 var inputfilename = WScript.Arguments(0);
+//
+// quit if inputfile doesn't exists
+//
+if ( ! oFso.FileExists(inputfilename) ) {
+	WScript.Echo("Inputfile [" + inputfilename + "] does not exists. Quitting.");
+	WScript.Quit(1);
+}
+
 var outputfilename = WScript.Arguments(1);
 //
 // !!! Attenzione !!! POS start at 1 !!!
@@ -22,7 +32,7 @@ for (var i=0; i <= pos_end-pos_start; i++) {
 //
 // process lines
 //
-var oFso = WScript.CreateObject("Scripting.FileSystemObject");
+
 var out = oFso.CreateTextFile(outputfilename, true);
 eachLine( inputfilename, function(line) {
 	out.WriteLine( 		line.substr(0, pos_start-1) 
